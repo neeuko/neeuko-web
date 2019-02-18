0acf188ecbca6fedcfd35271c3165ef3cacad8cf
Nelson L Nieves <nenievs@Nelsons-MacBook-Pro.local>
2019-02-18 11:52:47 -0400

    checkpoint 02

--- components/body-text.js
+++ components/body-text.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { H1, H2, H3, H4, P } from "../lib/typography";
+import { H1, H2, H3, H4, H5, P } from "../lib/typography";
 
 export const TitleBody = ({ children = "Body", title = "TITLE" }) => (
   <div>
-    <H2 style={{ textAlign: "center" }}>{title}</H2>
+    <H3 style={{ textAlign: "center" }}>{title}</H3>
     <P>{children}</P>
   </div>
 );
@@ -14,8 +14,10 @@ export const DateTitleBody = ({
   date = "Date"
 }) => (
   <div>
-    <H4 style={{ textAlign: "right" }}>{date}</H4>
-    <H3 style={{ textAlign: "center" }}>{title}</H3>
+    <H5 style={{ textAlign: "left" }}>{date}</H5>
+    <H4 style={{ textAlign: "left" }}>{title}</H4>
     <P>{children}</P>
   </div>
 );
+
+export const TitleOrderedBody = ({ children = "Body", title = "TITLE", })
--- pages/education/index.js
+++ pages/education/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../components/layout";
 import { H1 } from "../../lib/typography";
 import { TitleBody } from "../../components/body-text";
+import { CTA } from "../../components/buttons";
 
 const About = () => {
   const [initialized, setInitialized] = useState(false);
@@ -20,14 +21,26 @@ const About = () => {
   return (
     <Layout>
       <div>
-        <H1>NEEUKO WEB</H1>
-        <TitleBody title="WHAT WE DO">
-          At The Center we work at the intersection of academic research,
-          practical design, and art. It is organized as a structure of three
-          units: a design laboratory, a fabrication lab(FabLab), and a community
-          program(Nuestro Barrio); within those units we provide services in the
-          areas of ideation, validation, research, product development, and
-          commercialization.
+        <TitleBody title="INNOVATION">
+          Innovation is crucial for business development and incredibly
+          necessary for the betterment of society. Our innovation courses and
+          workshops are neatly designed to teach students and our immediate
+          community about how innovation process work and are implemented.
+        </TitleBody>
+        <TitleBody title="DESIGN">
+          At neeuko we pride ourselves on incorporating design in every aspect
+          of our decision making and production efforts. For us, design is not
+          only about making, is also about thinking, questioning, and
+          reinventing. Through our design courses we seek to inspire students to
+          think about design as a form of questioning.
+        </TitleBody>
+        <CTA onClick={() => console.log("success!")}>WORKSHOPS</CTA>
+        <TitleBody title="ENTREPENEURSHIP">
+          Being able to bring products to market can help people achieve
+          financial independence. For us ensuring financial independence is
+          incredibly important to that end we have models some important
+          workshops to ensure that students and the community have a shot at
+          undertake a business venture.
         </TitleBody>
       </div>
     </Layout>
