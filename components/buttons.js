a27fe6ba772070a8bfbed9671c75eb763218bf10
Nelson L Nieves <nenievs@Nelsons-MacBook-Pro.local>
2019-02-17 20:57:26 -0400

    create CTA Button

--- components/buttons.js
+++ components/buttons.js
@@ -1,1 +1,26 @@
-import { useState } from "react";
+import { Colors } from "../lib/colors";
+import { H2 } from "../lib/typography";
+
+export const CTA = props => {
+  return (
+    <button
+      type="button"
+      onClick={props.onClick}
+      style={{
+        backgroundColor: Colors.blue,
+        border: "none",
+        cursor: "pointer"
+      }}
+    >
+      <H2
+        color={Colors.white}
+        style={{
+          padding: "5pt 20pt",
+          margin: "0"
+        }}
+      >
+        {props.children}
+      </H2>
+    </button>
+  );
+};
